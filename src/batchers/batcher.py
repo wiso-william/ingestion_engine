5664bdfd25645cb1e38918a026e27378bb436367
wiso-william <williammancy0@gmail.com>
2026-07-16 16:35:03 +0200

    Implemented logging in batcher

--- src/batchers/batcher.py
+++ src/batchers/batcher.py
@@ -1,4 +1,7 @@
 from collections.abc import Iterable, Iterator
+import logging
+
+logger = logging.getLogger(__name__)
 
 def batcher(
     rows: Iterable[tuple],
@@ -6,12 +9,16 @@ def batcher(
 ) -> Iterator[list[tuple]]:
     batch: list[tuple] = []
 
+    logger.debug(f"Starting batcher with batch size {batch_size}")
+
     for row in rows:
         batch.append(row)
 
         if len(batch) == batch_size:
+            logger.debug(f"Yielding batch of size {batch_size}")
             yield batch
             batch = []
 
     if batch:
+        logger.debug(f"Yielding final batch of size {len(batch)}")
         yield batch
